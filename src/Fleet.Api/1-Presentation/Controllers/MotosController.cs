717f22345d253603ef58f60765ba18a8999bb8d7
Cainã Jucá <cainajuca@gmail.com>
2025-06-10 23:41:54 -0300

    feat: implemented Gets for Vehicle

--- src/Fleet.Api/1-Presentation/Controllers/MotosController.cs
+++ src/Fleet.Api/1-Presentation/Controllers/MotosController.cs
@@ -1,7 +1,10 @@
-﻿using Fleet.Api._2_Application.UseCases.VehicleUseCases.Register;
+﻿using Fleet.Api._2_Application.UseCases;
+using Fleet.Api._2_Application.UseCases.VehicleUseCases.Register;
+using Fleet.Api._3_Domain.Entities;
 using Fleet.Api._3_Domain.Interfaces.Repositories;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace Fleet.Api._1_Presentation.Controllers;
 
@@ -24,6 +27,46 @@ public class MotosController : ControllerBase
     }
 
     // TODO: implement authorization for these endpoints
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllVehicle([FromQuery] string? placa)
+    {
+        Expression<Func<Vehicle, bool>> predicate = x =>
+            string.IsNullOrEmpty(placa) || x.LicensePlate == placa;
+
+        Expression<Func<Vehicle, object>> selector = x => new
+        {
+            Identificador = x.Identifier,
+            Ano = x.Year,
+            Modelo = x.Model,
+            Placa = x.LicensePlate,
+        };
+
+        var vehicles = await _vehicleRepo.GetAllAsync(predicate, selector);
+
+        var output = BaseOutput<IEnumerable<object>>.Success(vehicles);
+
+        return Ok(output);
+    }
+
+    [HttpGet("{identificador}")]
+    public async Task<IActionResult> GetDeliverymanByUsername(string identificador)
+    {
+        var vehicle = await _vehicleRepo.GetByIdentifierAsync(identificador);
+
+        if (vehicle == null)
+            return NotFound(BaseOutput<string>.Failure("Moto não encontrada"));
+
+        var output = BaseOutput<object>.Success(new
+        {
+            Identificador = vehicle.Identifier,
+            Ano = vehicle.Year,
+            Modelo = vehicle.Model,
+            Placa = vehicle.LicensePlate,
+        });
+
+        return Ok(output);
+    }
 
     [HttpPost]
     public async Task<IActionResult> Register([FromBody] RegisterVehicleInput input)
