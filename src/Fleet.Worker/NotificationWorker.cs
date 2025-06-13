bb1ebd6d51cd84d2b185e4cb619ca88c68a0a938
Cainã Jucá <cainajuca@gmail.com>
2025-06-12 23:11:06 -0300

    feat(worker): implement background service to consume vehicle events and forward to API

---
+++ src/Fleet.Worker/NotificationWorker.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace Fleet.Worker;
+public class NotificationWorker : BackgroundService
+{
+    private readonly string _rabbitMqUrl;
+    private readonly string _queueName;
+    private readonly string _exchangeName;
+
+    private IConnection? _connection;
+    private IModel? _channel;
+    private readonly HttpClient _httpClient;
+
+    public NotificationWorker(string rabbitMqUrl, string queueName, string apiBaseUrl, string exchangeName)
+    {
+        _rabbitMqUrl = rabbitMqUrl;
+        _queueName = queueName;
+        _exchangeName = exchangeName;
+
+        _httpClient = new HttpClient { BaseAddress = new Uri(apiBaseUrl) };
+
+        Console.WriteLine($"[Worker] Service started successfully");
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        var factory = new ConnectionFactory { Uri = new Uri(_rabbitMqUrl) };
+
+        _connection = factory.CreateConnection();
+
+        _channel = _connection.CreateModel();
+
+        // Assures that the queue exists before consuming messages
+        _channel.QueueDeclare(
+            queue: _queueName,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        _channel.QueueBind(
+            queue: _queueName, // vehicles.2024.queue
+            exchange: _exchangeName, // vehicles.exchange
+            routingKey: "vehicle.created.2024");
+
+        return base.StartAsync(cancellationToken);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(_channel!);
+
+        consumer.Received += async (_, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            var payload = JsonSerializer.Serialize(new { Message = message });
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var resp = await _httpClient.PostAsync("NotificationMessage", content, stoppingToken);
+                if (resp.IsSuccessStatusCode)
+                    // only acknowledge the message if the API call was successful
+                    _channel!.BasicAck(ea.DeliveryTag, multiple: false);
+                else
+                    Console.WriteLine($"[Worker] API retornou {(int)resp.StatusCode}");
+                    // optional: let the message remain unacknowledged for retry
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Worker] Erro ao chamar API: {ex.Message}");
+            }
+        };
+
+        _channel!.BasicConsume(
+            queue: _queueName,
+            autoAck: false,
+            consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        _channel?.Close();
+        _connection?.Close();
+        _httpClient.Dispose();
+        base.Dispose();
+    }
+}
--- src/Fleet.Worker/Program.cs
+++ src/Fleet.Worker/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fleet.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        #region IIS
+        //var rabbitMqUrl = "amqp://guest:guest@localhost:5672";
+        //var apiBaseUrl = "https://localhost:44338/";
+        #endregion
+
+        #region Docker
+        var rabbitMqUrl = "amqp://guest:guest@rabbitmq:5672";
+        var apiBaseUrl = "http://api:8080";
+        #endregion
+
+        var queueName = "vehicles.2024.queue";
+        var exchangeName = "vehicles.exchange";
+
+        services.AddHostedService(sp =>
+            new NotificationWorker(rabbitMqUrl, queueName, apiBaseUrl, exchangeName));
+    })
+    .Build()
+    .Run();
