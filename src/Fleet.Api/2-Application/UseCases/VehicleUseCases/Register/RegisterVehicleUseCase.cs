a8621bb5f0459219922fdca459e27bd48af79297
Cainã Jucá <cainajuca@gmail.com>
2025-06-12 22:57:12 -0300

    feat(vehicle): publish VehicleCreated event when registering new vehicle

--- src/Fleet.Api/2-Application/UseCases/VehicleUseCases/Register/RegisterVehicleUseCase.cs
+++ src/Fleet.Api/2-Application/UseCases/VehicleUseCases/Register/RegisterVehicleUseCase.cs
@@ -1,19 +1,24 @@
 ﻿using Fleet.Api._3_Domain.Entities;
 using Fleet.Api._3_Domain.Interfaces.Repositories;
+using Fleet.Api._3_Domain.Interfaces.Services;
 using MediatR;
+using System.Text.Json;
 
 namespace Fleet.Api._2_Application.UseCases.VehicleUseCases.Register;
 
 public class RegisterVehicleUseCase : IRequestHandler<RegisterVehicleInput, RegisterVehicleOutput>
 {
     private readonly IVehicleRepository _vehicleRepository;
+    private readonly IMessagePublisher _publisher;
     private readonly ILogger<RegisterVehicleUseCase> _logger;
 
     public RegisterVehicleUseCase(
         IVehicleRepository vehicleRepository,
+        IMessagePublisher publisher,
         ILogger<RegisterVehicleUseCase> logger)
     {
         _vehicleRepository = vehicleRepository;
+        _publisher = publisher;
         _logger = logger;
     }
     public async Task<RegisterVehicleOutput> Handle(RegisterVehicleInput input, CancellationToken ct)
@@ -35,9 +40,28 @@ public class RegisterVehicleUseCase : IRequestHandler<RegisterVehicleInput, Regi
 
         // TODO: add domain validation for Vehicle
         await _vehicleRepository.AddAsync(vehicle);
-
         await _vehicleRepository.SaveChangesAsync();
 
+        await PublishVehicleRegistrationEventAsync(vehicle);
+
         return new RegisterVehicleOutput(true);
+    }
+
+    private async Task PublishVehicleRegistrationEventAsync(Vehicle vehicle)
+    {
+        var @event = new
+        {
+            vehicle.Id,
+            vehicle.Identifier,
+            vehicle.Model,
+            vehicle.Year,
+            vehicle.LicensePlate
+        };
+
+        var payload = JsonSerializer.SerializeToUtf8Bytes(@event);
+
+        var routingKey = $"vehicle.created.{vehicle.Year}";
+
+        await _publisher.PublishAsync(routingKey, payload);
     }
 }
